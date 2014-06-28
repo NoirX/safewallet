19d5afa215c373f47343372c7694917a541f023a
Noir Group <git@noirds.com>
2014-06-28 23:39:27 +0200

    More Noir stuff

--- config/dev.js
+++ config/dev.js
@@ -6,12 +6,40 @@ module['exports'] = {
   "coldstorage": {
     "bitcoin": [{ public_key: "148dBGs99rnFHSu3euZYziABse6Prs5L7J" }],
     "dogecoin": [{ public_key: "DS6p3PRcPrECMRzX37H92PFoifnrj717ZB" }],
+    "litecoin": [{ public_key: "148dBGs99rnFHSu3euZYziABse6Prs5L7J" }],
+    "noirshares": [{ public_key: "DS6p3PRcPrECMRzX37H92PFoifnrj717ZB" }],
+    "noirbits": [{ public_key: "148dBGs99rnFHSu3euZYziABse6Prs5L7J" }],
+    "noirtokens": [{ public_key: "DS6p3PRcPrECMRzX37H92PFoifnrj717ZB" }],
     "peercoin": [{ public_key: "PRsyinCxhkJ5ib7cWXzqTTYXN7mwJybtA4" }]
   },
   "deposits": {
     "bitcoin": {
       host: 'localhost',
       port: 8333,
+      user: 'username',
+      pass: 'password',
+    },
+    "litecoin": {
+      host: 'localhost',
+      port: 8334,
+      user: 'username',
+      pass: 'password',
+    },
+    "noirbits": {
+      host: 'localhost',
+      port: 8335,
+      user: 'username',
+      pass: 'password',
+    },
+    "noirshares": {
+      host: 'localhost',
+      port: 8336,
+      user: 'username',
+      pass: 'password',
+    },
+    "noirtokens": {
+      host: 'localhost',
+      port: 8337,
       user: 'username',
       pass: 'password',
     },
@@ -32,6 +60,30 @@ module['exports'] = {
     "bitcoin": {
       host: 'localhost',
       port: 9801,
+      user: 'username',
+      pass: 'password',
+    },
+    "litecoin": {
+      host: 'localhost',
+      port: 9804,
+      user: 'username',
+      pass: 'password',
+    },
+    "noirbits": {
+      host: 'localhost',
+      port: 9805,
+      user: 'username',
+      pass: 'password',
+    },
+    "noirtokens": {
+      host: 'localhost',
+      port: 9806,
+      user: 'username',
+      pass: 'password',
+    },
+    "noirshares": {
+      host: 'localhost',
+      port: 9807,
       user: 'username',
       pass: 'password',
     },
